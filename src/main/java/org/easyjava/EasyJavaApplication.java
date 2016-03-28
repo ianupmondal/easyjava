7296d6ce96da28c7af288b2d864ee59e51cb5c9d
Anup Mondal <anupmondal@Anups-MacBook-Air.local>
2016-03-29 01:40:00 +0530

    latest

--- src/main/java/org/easyjava/EasyJavaApplication.java
+++ src/main/java/org/easyjava/EasyJavaApplication.java
@@ -3,13 +3,10 @@ package org.easyjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class EasyJavaApplication {
 
-    
-
-    public static void main(String[] args) {
-        SpringApplication.run(EasyJavaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EasyJavaApplication.class, args);
+	}
 }
--- src/test/java/org/easyjava/EasyJavaApplicationTests.java
+++ src/test/java/org/easyjava/EasyJavaApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = EasyjavaApplication.class)
+@SpringApplicationConfiguration(classes = EasyJavaApplication.class)
 @WebAppConfiguration
-public class EasyjavaApplicationTests {
+public class EasyJavaApplicationTests {
 
 	@Test
 	public void contextLoads() {
